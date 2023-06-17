b08ae3ae59904717d3585a7cba2f308cb30a8e6e
Michael Olushola <goshenconsultsng@gmail.com>
2023-06-17 02:21:53 -0700

    Task 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdli.h>
+#include <time.h>
+/**
+ * main - A program that prints all single digit numbers of base 10
+ *
+ * Return Always (Success)
+ */
+
+int main(void)
+
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+	putchar('\n');
+
+	return (0);
+}
