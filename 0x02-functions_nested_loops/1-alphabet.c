8a7af529fddaef3403f1e4f7f01c2a16594b0759
Michael Olushola <goshenconsultsng@gmail.com>
2023-06-20 15:26:27 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/** Main - Print alphabet in lower cases
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+}
