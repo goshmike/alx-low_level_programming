ccb0f7b3c1d836996d660997d8b2d874a877db4d
Michael Olushola <goshenconsultsng@gmail.com>
2023-06-23 18:00:33 -0700

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line -straight -line
+ * @m:No of times
+ * Return:Always 0
+ */
+
+void print_line(int m)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
