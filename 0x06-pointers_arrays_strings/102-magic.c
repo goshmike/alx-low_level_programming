7177d63ce26b5c2f6ef90ac34f64ed91c7f5dcb0
Michael Olushola <goshenconsultsng@gmail.com>
2023-06-30 09:29:16 -0700

    Task 102

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -8,10 +8,9 @@ int *p;
 
 a[2] = 1024;
 p = &n;
-  /*
+  /**
    * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
+   * Remember:you are not allowed to use a
    * - you are not allowed to modify p
    * - only one statement
    * - you are not allowed to code anything else than this line of code
