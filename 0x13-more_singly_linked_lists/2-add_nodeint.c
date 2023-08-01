078756b6ebf5213ec5264329d63085204af39017
Michael Olushola <goshenconsultsng@gmail.com>
2023-08-01 14:57:35 -0700

    Task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -6,7 +6,7 @@
 /**
  * add_nodeint - adds a new node at the beginning of a linked list
  * @head: pointer to the first node in the list
- * @n: data to insert in that new node
+ * @p: data to insert in that new node
  *
  * Return: pointer to the new node, or NULL if it fails
  */
