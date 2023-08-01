f13beba302a3795efb7b7f60e128aa919f5fe2d4
Michael Olushola <goshenconsultsng@gmail.com>
2023-08-01 14:53:36 -0700

    Task 2

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
@@ -12,7 +14,7 @@ size_t num = 0;
 
 while (h)
 {
-printf("%d\n", h->n);
+printf("%d\n", h->p);
 num++;
 h = h->next;
 }
---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_nodeint - adds a new node at the beginning of a linked list
+ * @head: pointer to the first node in the list
+ * @n: data to insert in that new node
+ *
+ * Return: pointer to the new node, or NULL if it fails
+ */
+listint_t *add_nodeint(listint_t **head, const int p)
+{
+listint_t *new;
+
+new = malloc(sizeof(listint_t));
+if (!new)
+return (NULL);
+
+new->p = p;
+new->next = *head;
+*head = new;
+
+return (new);
+}
+
