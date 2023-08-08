6153352648cd65c73d585b330cb02716ea82c449
Michael Olushola <goshenconsultsng@gmail.com>
2023-08-08 06:54:32 -0700

    Task 100

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -274,10 +274,10 @@ void close_elf(int elf)
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	Elf64_Ehdr *header;
-	int o, r;
+	int u, k;
 
-	o = open(argv[1], O_RDONLY);
-	if (o == -1)
+	u = open(argv[1], O_RDONLY);
+	if (u == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
 		exit(98);
@@ -285,15 +285,15 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	header = malloc(sizeof(Elf64_Ehdr));
 	if (header == NULL)
 	{
-		close_elf(o);
+		close_elf(u);
 		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
 		exit(98);
 	}
-	r = read(o, header, sizeof(Elf64_Ehdr));
-	if (r == -1)
+	k = read(u, header, sizeof(Elf64_Ehdr));
+	if (k == -1)
 	{
 		free(header);
-		close_elf(o);
+		close_elf(u);
 		dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", argv[1]);
 		exit(98);
 	}
@@ -310,7 +310,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	print_entry(header->e_entry, header->e_ident);
 
 	free(header);
-	close_elf(o);
+	close_elf(u);
 	return (0);
 }
 
